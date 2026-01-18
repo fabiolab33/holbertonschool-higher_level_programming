d8becbb1597b7ae849a4b8cb744df49e14c45140
Fabiola Burgos <fburgos913@gmail.com>
2026-01-18 17:15:46 -0400

    Print numbers in decimal and hexadecimal

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# Print numbers from 0 to98 in decimal and hexadecimal format
+# Print numbers from 0 to 98
+# In decimal and hexadecimal format
 
 for n in range(0, 99):
     print("{} = 0x{:x}".format(n, n))
-    
