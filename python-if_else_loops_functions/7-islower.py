22b4aa71f4a71fa380c92b5a5edc5271327259f7
Fabiola Burgos <fburgos913@gmail.com>
2026-01-18 17:41:40 -0400

    Check for lowercase character

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Function to check if a character is lowercase
+
+def islower(c):
+    """
+    Returns True if character c is lowercase, False otherwise.
+    """
+    return ord(c) >= 97 and ord(c) <= 122
