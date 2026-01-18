92eaaacc74a43f0f84c97bc4f18f6e4a3d842424
Fabiola Burgos <fburgos913@gmail.com>
2026-01-17 20:20:02 -0400

    Print object-oriented programming with Python

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "object"
-str2 = "programming"
-str3 = "Python"
-print(f"{str1}-oriented {str2} with {str3}")
+str2 = "Python"
+print(f"{str1}-oriented programming with {str2}")
+
