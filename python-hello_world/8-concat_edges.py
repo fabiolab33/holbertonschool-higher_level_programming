b6ea282efb6515db88f02b225aae4ad47a6a59aa
Fabiola Burgos <fburgos913@gmail.com>
2026-01-17 20:51:33 -0400

    Print object-oriented programming with Python

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "object"
-str2 = "Python"
-print(f"{str[:]}-oriented programming with {str2}")
+str = "Python is an object-oriented programming language with unmatched power, thanks to its various libraries, among which is C"
+str = str[13:40] + str[50:56] + str[0:6]
+print(str)
 
