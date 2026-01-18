8c59196d457adbc71972d546328e6669e083a961
Fabiola Burgos <fburgos913@gmail.com>
2026-01-18 18:49:57 -0400

    Print result of 1 plus 2

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-add_0 = __import__('add_0').add
 
 if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    result = add_0(a, b)
-    print("{} + {} = {}".format(a, b, result))
+    print("{} + {} = {}".format(a, b, add(a, b)))
     
