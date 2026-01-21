54ab2f24ce09c2e06ff0984629cd9dabf7416af3
Fabiola Burgos <fburgos913@gmail.com>
2026-01-21 19:57:06 -0400

    Prints a dictionary by ordered keys

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    """
+    Prints a dictionary by ordered keys.
+
+    Args:
+        a_dictionary (dict): Dictionary to print
+    """
+    for key in sorted(a_dictionary.keys()):
+        print("{}: {}".format(key, _dictionary[key]))
