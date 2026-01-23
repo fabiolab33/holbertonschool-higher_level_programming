7e0800d53bff4e9f7ea14c1da202eb4c6a83a19c
Fabiola Burgos <fburgos913@gmail.com>
2026-01-23 11:48:35 -0400

    Raises a name exception with a message

---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Raises a NameError exception with a massage."""
+
+
+def raise_exception_msg(message=""):
+    """Raises a NameError with a custom message."""
+    raise NameError(message)
