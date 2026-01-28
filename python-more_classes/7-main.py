b5dbd4b400a7e6b2a89bf56c501970754f268efb
Fabiola Burgos <fburgos913@gmail.com>
2026-01-28 17:49:54 -0400

    Defines a rectangle based on file 6

---
+++ python-more_classes/7-main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+Rectangle = __import__('7-rectangle').Rectangle
+
+myrectangle1 = Rectangle(8, 4) 
+print(myrectangle1) 
+print("–") 
+
+myrectangle1.printsymbol = "&" 
+print(myrectangle_1) 
+print("–")
+
+myrectangle2 = Rectangle(2, 1) 
+print(myrectangle2) 
+print("–") 
+
+Rectangle.printsymbol = "C" 
+print(myrectangle_2) 
+print("–")
+
+myrectangle3 = Rectangle(7, 3) 
+print(myrectangle3)
+
+print("–")
+
+myrectangle3.printsymbol = ["C", "is", "fun!"] 
+print(myrectangle_3)
+
+print("–")
