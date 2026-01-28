5f3a7f5fd286960dbcca3b0eae8246f8a196d11b
Fabiola Burgos <fburgos913@gmail.com>
2026-01-27 20:21:42 -0400

    Defines a square

---
+++ python-classes/5-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Module that defines a Square class with a print method"""
+
+
+class Square:
+    """Class that defines a square and can print it"""
+
+    def __init__(self, size=0):
+        """Initialize the square
+
+        Args:
+            size (int): The size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation
+
+        Args:
+            value (int): The new size of the square
+
+        Raises:
+            TypeError: If value is not an integer
+            ValueError: If value is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculates the current square area
+
+        Returns:
+            The square of the size
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square with the # character to stdout
+
+        If size is 0, prints an empty line.
+        """
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
