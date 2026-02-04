a37bbcc1912a9752521efcbd7ac90c373ca5c713
Fabiola Burgos <fburgos913@gmail.com>
2026-02-04 15:57:57 -0700

    Returns the list of available attributes and methods of an object

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,10 +4,10 @@
 
 def lookup(obj):
     """Returns the list of available attributes and methods of an object
-    
+
     Args:
         obj: The object to inspect
-        
+
     Returns:
         list: A list of strings representing attributes and methods
     """
