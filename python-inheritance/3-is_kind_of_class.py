edb9b19499fcfb9082f80f6dd60f1819e3b3eb44
Fabiola Burgos <fburgos913@gmail.com>
2026-02-04 23:11:34 -0700

    Function that returns True

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that contains the function is_kind_of_class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True if obj is an instance of, or inherited from, a_class
+    
+    Args:
+        obj: The object to check
+        a_class: The class to compare against
+    
+    Returns:
+        bool: True if matches, otherwise False
+    """
+    return isinstance(obj, a_class)
