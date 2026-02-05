03b0afc51928401c68b296e30834b2e641cc0814
Fabiola Burgos <fburgos913@gmail.com>
2026-02-04 23:23:19 -0700

    Function that returns True

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that contains the function inherits_from"""
+
+
+def inherits_from(obj, a_class):
+    """Return True if obj is an instance of a class that inherited from a_class
+    
+    Args:
+        obj: The object to check
+        a_class: The class to compare against
+    
+    Returns:
+        bool: True if it inherits and is not the same class, otherwise False
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
