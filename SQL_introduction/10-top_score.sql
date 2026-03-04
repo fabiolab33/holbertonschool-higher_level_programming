0e2fd0d3f033b83789a3effa0bb57f930af6a80f
Fabiola Burgos <fburgos913@gmail.com>
2026-03-04 13:55:57 -0700

    List by best

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- List all records of second_table ordered by score descending
+-- This script displays score and name ordered from highest to lowest score
+SELECT score, name FROM second_table ORDER BY score DESC;
