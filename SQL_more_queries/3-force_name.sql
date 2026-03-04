84db077f740f24301f99fa10ed24940b8aafa8e4
Fabiola Burgos <fburgos913@gmail.com>
2026-03-04 15:43:52 -0700

    Always a name

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,8 @@
+-- 3-force_name.sql
+-- Create the table force_name with id INT and name VARCHAR(256) NOT NULL
+-- Table should not fail if it already exists
+
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
