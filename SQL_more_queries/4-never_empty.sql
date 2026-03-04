77aec9493e834c26380d66b6eb8c01037ba26576
Fabiola Burgos <fburgos913@gmail.com>
2026-03-04 15:46:54 -0700

    ID can t be null

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,8 @@
+-- 4-never_empty.sql
+-- Create the table id_not_null with id default value 1
+-- Table should not fail if it already exists
+
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);
