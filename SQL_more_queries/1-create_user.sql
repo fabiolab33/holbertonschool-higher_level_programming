8f5463399300162994d0effe8890ef6d73850724
Fabiola Burgos <fburgos913@gmail.com>
2026-03-04 15:10:32 -0700

    All privileges

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+-- 1-create_user.sql
+-- Create the MySQL user 'user_0d_1' with all privileges on localhost
+-- If the user already exists, do not fail
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+FLUSH PRIVILEGES;
