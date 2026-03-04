b90ba4921c35c12d4a43d72b49d8692080c0bf89
Fabiola Burgos <fburgos913@gmail.com>
2026-03-03 21:26:29 -0700

    Full description

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Display the full description of first_table from the database passed as argument
+-- This shows the CREATE TABLE statement for first_table without using DESCRIBE or EXPLAIN
+SHOW CREATE TABLE first_table;
