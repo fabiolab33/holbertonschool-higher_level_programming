5f04584ee7673c42079192467978f0d73109c931
Fabiola Burgos <fburgos913@gmail.com>
2026-03-04 15:49:27 -0700

    Unique ID

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,8 @@
+-- 5-unique_id.sql
+-- Create the table unique_id with id default value 1 and unique constraint
+-- Table should not fail if it already exists
+
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
